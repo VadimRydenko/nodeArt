699c5dee7f9999cfeb360fd2243d32f362cc9ad1
VadimRydenko <rudenkovvv@ukr.net>
2018-11-25 15:50:39 +0200

    add content

--- src/index/index.js
+++ src/index/index.js
@@ -1,1 +1,29 @@
 import './index.scss';
+
+// const button = document.querySelector('.leftblock-menu__button');
+// const container = document.querySelector('.leftblock-menu__container');
+
+// for (let i = 0 ; i<button.length; i++) {
+//     button[i].onclick = toogle;
+// }
+
+// function toogle() {
+//     switchOff();
+//     this.classList.add('leftblock-menu__container-open');
+// }
+
+// function switchOff() {
+//     for (let i = 0 ; i<container.length; i++) {
+//         contsiner[i].classList.remove('leftblock-menu__container-open');
+//     }
+// }
+
+
+
+// // function close () {
+// //     container.classList.remove('leftblock-menu__items-open');
+// // }
+// // function open () {
+// //     console.log(this);
+// //     container.classList.add('leftblock-menu__items-open');
+// // }
